de7fbb8a848815abe7be1afb21c427b9f7f6695a
JustCookieDough <scottangelides@gmail.com>
2024-03-12 23:16:00 -0400

    added full flask front-end and title file loading and hashtable and a bunch of other shiz,,, didn't touch the graph stuff tho

---
+++ src/app.py
@@ -0,0 +1,50 @@
+# imports
+from flask import Flask, render_template, request
+from search import Database
+
+# setting up database and app
+db = Database("../datasets/edges.txt.gz", "../datasets/titles.txt.gz")
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/solution')
+def solution():
+    start_page = request.args.get('page')
+
+    try:
+        start_id = db.get_id_from_name(start_page)
+    except:
+        return render_template("bad-page.html", page=start_page)
+    
+    try:
+        path_ids = db.get_path(start_id)
+    except:
+        return render_template("no-path.html", page=start_page)
+
+    return render_template("solution.html", sites=ids_to_sites_dicts(path_ids), clicks=len(path_ids), start=start_page)
+
+
+def ids_to_sites_dicts(ids: list[id]) -> list[dict[str, str]]:
+    """
+    Converts a list of names into a list of dicts in the shape of:
+    {"name": [page_name], "url": [page_url]}
+    where page_name is the name passed into the function,
+    and page_url is the Wikipedia URL for that page.
+
+    See https://en.wikipedia.org/wiki/Help:URL for more info
+    """
+    out_list = []
+    for id in ids:
+        name = db.get_name_from_id(id)
+        url = "https://en.wikipedia.org/wiki/" + name.replace(" ", "_")
+        out_list.append({"name": name, "url": url})
+    return out_list
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ src/search.py
@@ -0,0 +1,63 @@
+
+import gzip
+
+
+class Node():
+    """
+    # TODO: figure this jawn out
+    """
+    def __init__(self) -> None:
+        pass
+
+
+class Database():
+    """
+    # TODO: write a docstring and also finish this lmao
+    """
+    def __init__(self, edge_file_path, title_file_path) -> None:
+        self._titles = self._load_titles(title_file_path)
+        self._pages = self._load_edges(edge_file_path)
+
+
+    def _load_titles(self, path: str) -> dict[int, str]: # slow and bad but works
+        """
+        loads the titles into a dict: {id: name}
+
+        # TODO: write better docstring
+        """
+        out_dict = {}
+        with gzip.open(path, "r") as file:
+            for line in file:
+                data = str(line, 'utf-8').strip().split(" ")
+                out_dict[int(data[0])] = " ".join(data[1:])
+        return out_dict        
+
+
+    def _load_edges(self, path: str) -> list[Node]:
+        # TODO: write this
+        pass
+
+
+    def get_name_from_id(self, id: int) -> str:
+        """
+        Returns a page's name when given its id
+        """
+        if id in self._titles:
+            return self._titles[id]
+        raise KeyError("name not in titles database")
+
+
+    def get_id_from_name(self, name: str) -> int:
+        """
+        Returns a page's id when given its name
+        """
+        for key in self._titles:
+            if self._titles[key] == name:
+                return key
+        raise KeyError("id not in titles database")
+
+
+    def get_path(self, id: int) -> list[int]:
+        # TODO: implement this (lmao easier said than done)
+        raise KeyError("no path!")
+        return [id]  # just to see if the front end is working
